2b067ce3dc20e41b687f5503fea53ea2d025fe00
WWWWPPPJJ <60551893+warinthon30@users.noreply.github.com>
2021-09-06 11:04:38 +0700

    update-struvtur

--- src/pages/add-coin/CoinMake.vue
+++ src/pages/add-coin/CoinMake.vue
@@ -1,16 +1,87 @@
 <template>
-    <div class="q-pa-xl">
-        <span class="text-h2 text-weight-bold">สร้างสูตรผสม COIN</span>
- 
+  <div class="q-pa-xl">
+    <div class="row">
+      <div class="col-sm-9" style="max-width: 700px">
+        <strong>สร้างสูตรผสม COIN</strong>
+        <!--
+          <q-btn flat @click="drawerRight = !drawerRight" round dense icon="save" /> 
+          -->         
+        <div class="row">
+          <div class="col-8 col-sm-6">
+              <q-input outlined v-model="text1" placeholder="Campaign Name or Prize" > 
+                  <template v-slot:prepend>
+                   <q-icon name="search" />
+                  </template>
+           </q-input>           
+           </div>
+           <div class="col-4 col-sm-6">
+           <q-input v-model="date1" filled type="date" hint="" />
+           </div>
+           </div>
+           <div class="col-4 col-sm-6"> 
+           </div>
+           <!--
+         <div class="row reverse">   
+      <div class="col">
+        <strong>Perios</strong>
+      </div>
+      <div class="col">
+       <strong>Prize</strong>
+      </div>
+      <div class="col" >
+        <strong>Campaign Name</strong>
+      </div>
     </div>
+   -->
+
+      <CoinDrawer/>
+      </div>
+    </div>
+     
+  </div>
 </template>
 
 <script>
-export default {
+import { ref } from 'vue'
+import CoinDrawer from './CoinDrawer.vue'
 
+export default {
+  components:{
+    CoinDrawer
+  },
+  setup () {
+     function linkClick (e, go) {
+      e.preventDefault() // we choose when we navigate
+
+      // console.log('triggering navigation in 2s')
+      setTimeout(() => {
+        // console.log('navigating as promised 2s ago')
+        go()
+      }, 2000)
+    }
+    
+    return {
+      linkClick,
+      alert: ref(false),
+      confirm: ref(false),
+      prompt: ref(false),
+      model1: ref(),
+      model2: ref(),
+      address: ref(''),
+      date1: ref(''),
+      date2: ref(''),
+      columns:ref(''),
+      rows:ref(''),
+      text1: ref(''),
+      text2: ref(''),
+      text3: ref(''),
+      text4: ref(''),
+      ph: ref(''),
+      dense: ref(false),
+      drawerRight: ref(false)
+
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
--- src/pages/add-prize/PrizeMake.vue
+++ src/pages/add-prize/PrizeMake.vue
@@ -1,16 +1,68 @@
 <template>
-    <div class="q-pa-xl">
-        <span><strong>รางวัล</strong></span>
-
+  <div class="q-pa-xl">
+    <div class="row">
+      <div class="col-sm-9" style="max-width: 700px">
+        <strong>รางวัล</strong>
+             
+        <div class="row">
+          <div class="col-8 col-sm-6">
+              <q-input outlined v-model="text1" placeholder="ชื่อรางวัล/รายละเอียด" > 
+                  <template v-slot:prepend>
+                   <q-icon name="search" />
+                  </template>
+           </q-input>           
+           </div>    
+           </div>
+           <div class="col-4 col-sm-6"> 
+           </div>
+      <CoinDrawer/>
+      </div>
     </div>
+     
+  </div>
 </template>
 
 <script>
-export default {
+import { ref } from 'vue'
+import CoinDrawer from './PrizeDrawer.vue'
 
+export default {
+  components:{
+    CoinDrawer
+  },
+  setup () {
+     function linkClick (e, go) {
+      e.preventDefault() // we choose when we navigate
+
+      // console.log('triggering navigation in 2s')
+      setTimeout(() => {
+        // console.log('navigating as promised 2s ago')
+        go()
+      }, 2000)
+    }
+    
+    return {
+      linkClick,
+      alert: ref(false),
+      confirm: ref(false),
+      prompt: ref(false),
+      model1: ref(),
+      model2: ref(),
+      address: ref(''),
+      date1: ref(''),
+      date2: ref(''),
+      columns:ref(''),
+      rows:ref(''),
+      text1: ref(''),
+      text2: ref(''),
+      text3: ref(''),
+      text4: ref(''),
+      ph: ref(''),
+      dense: ref(false),
+      drawerRight: ref(false)
+
+    }
+  }
 }
 </script>
 
-<style>
-
-</style>
